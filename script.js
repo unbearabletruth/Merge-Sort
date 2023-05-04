289235bffe37b06b5754f88255b5f7131efafac5
unbearabletruth <antaarty@gmail.com>
2023-05-04 11:54:11 +0700

    pseudocode set up

---
+++ script.js
@@ -0,0 +1,9 @@
+function mergeSort(array){
+    //1) base case to quit recursive loop
+    //2) recursively sort left side of array
+    //3) recursively sort right side of array
+    //4) merge them together
+    //5) recursive tree is simular to the Fibonacci recursive tree
+}
+
+console.log(mergeSort([3,9,22,1,7,44,2,11]))
