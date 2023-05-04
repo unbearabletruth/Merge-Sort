24e4d61177e72a39dcd2b71f89f478442d456b38
unbearabletruth <antaarty@gmail.com>
2023-05-04 15:29:25 +0700

    works
    had to look up merge part

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function mergeSort(array){
-    if (array.length < 2) return array[0];
+    if (array.length < 2) return array;
     const half = Math.ceil(array.length / 2);
     const left = array.slice(0, half); 
     const right = array.slice(half);
-    mergeSort(left);
-    mergeSort(right);
-    //if (Number(left) < Number(right)) return left.concat(right);
-    //else return right.concat(left);
-    for (let i = 0; i < array.length; i++) {
-        if (left[i] > right[i]) return [right[i]].concat(left[i]);
-        else if (left[i] < right[i]) return [left[i]].concat(right[i]); 
+    const mergeLeft = mergeSort(left);
+    const mergeRight = mergeSort(right);
+    let sortedArray = [];
+    while (mergeLeft.length && mergeRight.length) {
+        if (mergeLeft[0] < mergeRight[0]) {
+            sortedArray.push(mergeLeft.shift());
+        } else {
+            sortedArray.push(mergeRight.shift());
+        }
     }
+    return [...sortedArray, ...mergeLeft, ...mergeRight]
 }
 
-console.log(mergeSort([3,9,22,1,7,44,2,11]))
-
-
+console.log(mergeSort([3,9,22,1,7,44,2,11]));
+console.log(mergeSort([9,2,4,7,3]));
